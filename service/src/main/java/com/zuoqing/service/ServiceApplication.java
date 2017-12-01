33deaf01279a751a8498cff837f1dfc61b8ccbd0
zuoqing <zuoqing135du@163.com>
2017-12-01 20:47:07 +0800

    增加redis

--- service/src/main/java/com/zuoqing/service/ServiceApplication.java
+++ service/src/main/java/com/zuoqing/service/ServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = {"com.zuoqing.base.dao"}, sqlSessionFactoryRef = "sqlSessionFactory")
+@MapperScan(basePackages = {"com.zuoqing.base.dao"})
 public class ServiceApplication {
 
 	public static void main(String[] args) {
--- web/src/main/java/com/zuoqing/web/controller/LoginController.java
+++ web/src/main/java/com/zuoqing/web/controller/LoginController.java
@@ -1,10 +1,16 @@
 package com.zuoqing.web.controller;
 
+import com.zuoqing.base.entity.Girl;
 import com.zuoqing.base.entity.User;
+import com.zuoqing.web.redis.RedisService;
 import com.zuoqing.web.service.LoginConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -21,7 +27,7 @@ public class LoginController {
     @RequestMapping(value = "login")
     public Object login(){
         loginService.login();
-        return   "111";
+        return   "11122333444";
     }
 
     @RequestMapping(value = "findUser")
@@ -39,6 +45,52 @@ public class LoginController {
 //    public Object findMenuList(){
 //        return userService.findMenuList();
 //    }
+
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping(value = "/save", method = {RequestMethod.GET, RequestMethod.POST})
+    public String redisTest() {
+
+        List<Girl> list = new ArrayList();
+        Girl girl = new Girl();
+        girl.setCupSize("C");
+        girl.setAge(30);
+        list.add(girl);
+        redisService.set("girlList",list);
+
+        StringBuffer sb = new StringBuffer();
+        redisService.set("str", "str");
+        sb.append("str=").append(redisService.get("str").toString()).append(",");
+        redisService.hmSet("hmset", "key", "val");
+        sb.append("hmset=").append(redisService.hmGet("hmset", "key")).append(",");
+        redisService.lPush("list", "val");
+        sb.append("list=").append(redisService.lRange("list", 0, 1).toString()).append(",");
+        redisService.add("set", "val");
+        sb.append("set=").append(redisService.setMembers("set").toString()).append(",");
+        redisService.zAdd("zset", "val1", 1);
+        redisService.zAdd("zset", "val2", 2);
+        sb.append("zset=").append(redisService.rangeByScore("zset", 1, 2)).append(",");
+
+        return sb.toString();
+    }
+
+    @RequestMapping(value = "/get", method = {RequestMethod.GET, RequestMethod.POST})
+    public String redisTest2() {
+        StringBuffer sb = new StringBuffer();
+        redisService.set("str", "str");
+        sb.append("str=").append(redisService.get("str").toString()).append(",");
+        redisService.hmSet("hmset", "key", "val");
+        sb.append("hmset=").append(redisService.hmGet("hmset", "key")).append(",");
+        redisService.lPush("list", "val");
+        sb.append("list=").append(redisService.lRange("list", 0, 1).toString()).append(",");
+        redisService.add("set", "val");
+        sb.append("set=").append(redisService.setMembers("set").toString()).append(",");
+        redisService.zAdd("zset", "val1", 1);
+        redisService.zAdd("zset", "val2", 2);
+        sb.append("zset=").append(redisService.rangeByScore("zset", 1, 2)).append(",");
+        return sb.toString();
+    }
 
 
 }
